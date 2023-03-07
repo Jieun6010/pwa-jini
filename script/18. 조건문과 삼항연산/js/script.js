42843a739eb740cf1718d9646ca3ab08ac66f2e4
jeuny <wldms8997@gmail.com>
2023-03-07 18:18:23 +0900

    수정44

--- script/18. 조건문과 삼항연산/js/script.js
+++ script/18. 조건문과 삼항연산/js/script.js
@@ -2,7 +2,7 @@ $(function () {
   // ex1 ----------------------------------------------------------------- 
   $(`.ex1 button`).click(function () {
     var num1_1 = $(`.ex1 .num1_1`).val() //input이니까 val()
-    var num1_2 = $(`.ex1 .num1_2`).val()
+    var num1_2 = $(`.ex1 .num1_2`).val() // ''문자열이 들어올거야!
     var num1 = parseInt(num1_1 + num1_2)
     // 글자를 이어붙인거니까 숫자로 바꿔주기
     var num2_1 = $(`.ex1 .num2_1`).val()
@@ -11,19 +11,24 @@ $(function () {
     var oper = $(`.ex1 select option:selected`).text()
     // 밸류가 없는 옵션에서 바로 가져올땐 :selected(가상클래스) 를 붙여줘야한다. 또한 문자열이다!
 
+
+    // if (!false){return false}
+
+    //  !true
+
     // 
-
-
-
-    // 빈칸이 있을 시 경고 출력
-    if(!num1_1 || !num1_2 || !num2_1 || !num2_2 || !(num1_1-num1_2)){
+    if (
+      (!(num1_1 - num1_2) && num1_1 - num1_2 !== 0)
+      ||
+      (!(num2_1 - num2_2) && num2_1 - num2_2 !== 0)
+    ) { // 0이 아니면서 이상할때...
       alert('입력오류')
       return false
     }
-    
-    
+
+
     // 나누기에 0을 넣으면  경고 출력
-    if (oper === '/' &&  num2 === 0) {
+    if (oper === '/' && num2 === 0) {
       alert('0으로는 나눌 수 없어요')
       return false
     }
@@ -42,4 +47,59 @@ $(function () {
     }
     $(`.ex1 .output`).text(result)
   })
+
+  $(`.ex2 button`).click(function () {
+    var number = parseInt($(`.ex2 input`).val())
+
+    // 빈칸이나 한글을 넣었을 때 경고창 출력
+    if (!number && number !== 0) {
+      // 숫자가 아니면서 0은 아닐때
+      alert('숫자만 입력해주세요')
+      return false
+    }
+
+
+    /*
+    // 정답 / 오답 조건 만들기
+    var result
+    if (number > 10 && number % 2 === 0) {
+      result = '정답입니다.'
+    } else {
+      result = '오답입니다.'
+    }
+    $(`.ex2 .output`).text(result)
+    })
+    */
+
+    // 정답 / 오답같은 간단한 식은 이렇게 하는게 편하다.
+    //위의 식을 삼항연산으로 바꾸면 이렇게 간단한 식이 된다.
+    var result = (number > 10 && number % 2 === 0) ? `정답입니다` : `오답입니다.`
+    // True일땐 앞쪽 : flase일땐 뒷쪽
+    $(`.ex2 .output`).text(result)
+  })
+
+
+  /* ex3 ----------------------------------*/
+
+  var rnd = Math.ceil(Math.random() * 3)
+  // 랜덤함수 만들기
+  $(`.ex3 b`).text(rnd)
+  // 문제에 랜덤숫자 출력
+
+  // ex3의 버튼을 클릭했을때의 조건 
+  $(`.ex3 button`).click(function () {
+    var txt = $(`.ex3 input`).val()
+
+      
+    var result = ((txt === 1 && txt === 'one')||(txt === 2 && txt==='two')||(txt === 3 && txt === 'three')) ? `정답입니다` : `오답입니다.`
+    $(`.ex3 .output`).text(result)
+  })
+/* ex4 ----------------------------------------------------------- */
+  $(`.ex4 input`).click(function(){
+    var str = ($(this).val()==='o')?'x':'o'
+    console.log(str);
+    $(this).val(str)
+  })
+
+
 })
---
+++ script/19. 증감연산자/js/script.js
@@ -0,0 +1,79 @@
+// 이렇게 클릭함수 밖에서 변수 선언을 해줘야 한다. 안에서 변수선언을 하면 계속 같은 값만 나옴 
+/* 
+증감연산은 초기값이 필요함
+초기값 선언 위치가 중요함
+
+증감연산자
+n += 2 // n = n + 2 둘이 똑같은 말이다 !
+n ++ //n += 1  -> n이 1씩 증가한다. / 
+++ n // n += 1 
+
+n++ n을 넘겨주고나서 증가한다
+++ n 증가한 연산을 끝내고 넘겨준다. 단순히 쓸 때는 문제가 없지만
+리턴을 한다던가 값을 다른곳에 넘겨줄때는 반드시 ++n을 써야한다. 
+
+증감 > 검사 > 호출 의 순서로 사용해야한다.
+*/
+
+
+
+
+/*
+var n = 1
+// 클릭할 때마다 1씩 증가 // 5에 도달하면 멈추기.
+$(`.ex1 button`).click(function () {
+  ++ n // 함수로 묶을때는 이 식을 쓰지 못한다.
+  if(n > 5) n = 5
+  $(`.ex1 .output`).text(n)
+})
+*/
+
+$(function () {
+  var n = 1
+  //fn
+  function fn(param) {
+    n += param
+    if (n > 5) n = 5
+    if (n < -5) n = -5
+    $(`.ex1 .output`).text(n)
+  }
+  // 증가버튼
+  $(`.ex1 .increase`).click(function () {
+    fn(1)
+  })
+
+  // 감소버튼
+  $(`.ex1 .decrease`).click(function () {
+    fn(-1)
+  })
+})
+
+// 스코프 목적으로 ready이벤트 다시 선언
+$(function(){
+  var n = 1
+
+  function fn(param) {
+    n += param
+    if (n > 5) n = 1
+    if (n < 1) n = 5
+    $(`.ex2 .output`).text(n)
+  }
+  // 증가버튼
+  $(`.ex2 .increase`).click(function () {
+    fn(1)
+  })
+
+  // 감소버튼
+  $(`.ex2 .dicrease`).click(function () {
+    fn(-1)
+  })
+})
+
+$(function(){
+  var rdn
+  rdn = Math.floor(Math.random()*10)
+  $(`.ex3_1 .num1`).val(rdn)
+  rdn = Math.floor(Math.random()*10)
+  $(`.ex3_1 .num2`).val(rdn)
+
+}) //ready
